1794e481d5ceaa334b10db176d9d9c65e3e9e8b3
Rajat Talwar <cuterajat26@gmail.com>
2019-01-21 16:16:58 +0530

    add talks page

---
+++ src/routes/talks/route.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const path = require("path");
+const dirPath = "drafts";
+let venuePaths = fs.readdirSync(path.join(__dirname, dirPath));
+// let paths = require.resolve(paths);
+let paramsFromDirectory = [];
+for (let venue of venuePaths) {
+    let p = path.join(__dirname, dirPath, venue);
+    let talkPaths = fs.lstatSync(p).isDirectory() ? fs.readdirSync(p) : [];
+    for (let talk of talkPaths) {
+        talk.endsWith('.marko') && paramsFromDirectory.push({ venue: venue, title: talk.replace(".marko", "") });
+    }
+}
+console.log(paramsFromDirectory);
+exports.path = '/talks/:venue/:title';
+exports.params = paramsFromDirectory;
+exports.handler = (input, out) => {
+    let { venue, title } = input.params;
+    const template = require(`./${dirPath}/${input.params.venue}/${input.params.title.toLowerCase()}.marko`);
+    template.render({ venue, title }, out);
+};
